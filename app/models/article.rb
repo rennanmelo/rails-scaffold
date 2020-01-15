867982eb89c6373a28a77e97db4714ea27e70edb
Rennan Melo <rennan.melo11@gmail.com>
2020-01-15 20:49:17 -0200

    Create an article temporal test to verify if an article is archived

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,5 @@
 class Article < ApplicationRecord
+  def archived?
+    Date.current > self.created_at + 30.days
+  end
 end
--- test/helpers/articles_helper_test.rb
+++ test/helpers/articles_helper_test.rb
@@ -5,4 +5,14 @@ class ArticlesHelperTest < ActionView::TestCase
     article = articles(:uncap)
     assert_dom_equal %{<a href="/articles/#{article.id}">Uncapitalized title</a>}, link_to_article(article)
   end
+
+  test "should be archived after 30 days" do
+    first = Article.first
+    assert_not first.archived?
+
+    future = Date.current + 31.days
+    travel_to future do 
+      assert first.archived?
+    end
+  end
 end
